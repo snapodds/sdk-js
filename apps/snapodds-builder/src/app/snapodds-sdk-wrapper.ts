74629cf7b8089ef9172da4575d71370193af592a
Alexander Suslov <alexander.suslov@snapscreen.com>
2022-02-15 13:28:57 +0700

    Rename builders

--- apps/snapodds-builder/src/app/snapodds-sdk-wrapper.ts
+++ apps/snapodds-builder/src/app/snapodds-sdk-wrapper.ts
@@ -1,13 +1,13 @@
-import { BannerSdkBuilder } from './banner-sdk.builder';
+import { SportsMediaSdkBuilder } from './sports-media-sdk.builder';
 import { OddsSdkBuilder } from './odds-sdk.builder';
 import { SdkBuilder } from './sdk-builder';
-import { SnapSdkBuilder } from './snap-sdk.builder';
+import { OperatorsSdkBuilder } from './operators-sdk.builder';
 
 /**
  * Entry point for the user to interact with the different SDK Builders
  */
 export class SnapoddsSdkWrapper {
-  static bannerBuilder: () => SdkBuilder = () => new BannerSdkBuilder();
+  static sportsMediaBuilder: () => SdkBuilder = () => new SportsMediaSdkBuilder();
+  static operatorsBuilder: () => SdkBuilder = () => new OperatorsSdkBuilder();
   static oddsBuilder: () => SdkBuilder = () => new OddsSdkBuilder();
-  static snapBuilder: () => SdkBuilder = () => new SnapSdkBuilder();
 }
